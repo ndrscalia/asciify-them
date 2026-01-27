c46b6f8fbf79430d0648f5c6c44193c4c79bbdbd
ndrscalia <andrea95scalia@gmail.com>
2026-01-27 18:27:31 +0100

    update version to 1.1.0

--- src/asciify/__init__.py
+++ src/asciify/__init__.py
@@ -5,8 +5,16 @@ from typing import List
 from .core import asciify
 from .process import ImgProcessor
 from .renderer import Renderer
-from .utils import DEFAULT_CHARSET
+from .utils import BRAILLE_CHARSET, CLASSIC_GRADIENT, DEFAULT_CHARSET, EXTENDED_SMOOTH_GRADIENT, UNICODE_BLOCKS
 
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 
-__all__: List[str] = ["asciify", "ImgProcessor", "Renderer", "DEFAULT_CHARSET"]
+__all__: List[str] = ["asciify",
+                      "ImgProcessor",
+                      "Renderer",
+                      "DEFAULT_CHARSET",
+                      "CLASSIC_GRADIENT",
+                      "EXTENDED_SMOOTH_GRADIENT",
+                      "UNICODE_BLOCKS",
+                      "BRAILLE_CHARSET",
+                      ]
